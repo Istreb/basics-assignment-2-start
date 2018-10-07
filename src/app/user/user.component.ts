53131a4bc255993dcc80be5e97733bae1b878ff0
konstantin.kalichava <konstantin.kalichava@gotechsoftware.com>
2018-10-07 19:24:27 +0300

    all done

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,20 @@
 import {Component} from '@angular/core';
 
 @Component ({
-
+  selector: 'app-username',
+  templateUrl: 'user.component.html'
 })
+
+export class UserComponent {
+  username = '';
+  AllowClickOnTheButton = true;
+
+  onClearUsername() {
+    this.username='';
+  }
+  ifTheButtonCanBeClicked(){
+    if (this.username == '') {return true; }
+    else return false;
+  }
+
+}
